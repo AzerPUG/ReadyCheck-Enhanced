57741049476c5b5d1c2bc845ae17dfda069fa36f
Veltrex666 <Veltrex@live.nl>
2020-09-04 11:00:27 +0200

    Combined OnLoad, OnEvent and VersionControl namespaces with other modules. No more leakage into the Global space. Removed old code, now refers to HelperFunctions of CoreModule instead. Shortened the name in the .toc file and thus in the ingame addon panel.

--- main.lua
+++ main.lua
@@ -9,17 +9,17 @@ local addonMain = LibStub("AceAddon-3.0"):NewAddon("InstanceUtility-ReadyCheck",
 
 local readyCheckDefaultText = nil
 
-function VersionControl:ReadyCheckVC()
+function AZP.IU.VersionControl:ReadyCheck()
     return AZPIUReadyCheckVersion
 end
 
-function OnLoad:ReadyCheckOL(self)
+function AZP.IU.OnLoad:ReadyCheck(self)
     addonMain:ChangeOptionsText()
 
     InstanceUtilityAddonFrame:RegisterEvent("READY_CHECK")
     InstanceUtilityAddonFrame:RegisterEvent("UNIT_AURA")
 
-    addonMain:DelayedExecution(5, function()
+    AZP.AddonHelper:DelayedExecution(5, function()
         local pointY, relativeToY, relativePointY, xY, yY = ReadyCheckFrameYesButton:GetPoint()
         local pointN, relativeToN, relativePointN, xN, yN = ReadyCheckFrameNoButton:GetPoint()
         ReadyCheckFrame:SetSize(300, 150)
@@ -62,18 +62,24 @@ function addonMain:CheckConsumables(inputFrame)
     local collorGreen = "\124cFF00FF00"
     local colorEnd = "\124r"
 
-    --Strip first line of the current readycheck text, which says: X has initiated ready check.
+    local BuffsLabel = CreateFrame("Frame", "BuffsLabel", ReadyCheckFrame)
+    BuffsLabel:SetSize(100, 100)
+    BuffsLabel:SetPoint("TOP", 0, -30)
+    BuffsLabel.contentText = BuffsLabel:CreateFontString("BuffsLabel", "ARTWORK", "GameFontNormal")
+    BuffsLabel.contentText:SetPoint("TOP", 0, 0)
+    BuffsLabel.contentText:SetJustifyH("LEFT")
+
     readyCheckDefaultText = inputFrame:GetText()
     local endOfLine, _ = string.find(readyCheckDefaultText, "\n")
     if endOfLine ~= nil then
         readyCheckDefaultText = string.sub(readyCheckDefaultText, 1, endOfLine -1 )
     end
 
-    inputFrame:SetSize(400, 150)
-    inputFrame:SetPoint("TOP", 0, 20)
+    inputFrame:SetSize(250, 25)
+    inputFrame:SetPoint("TOP", 0, 0)
 
     local buffs, i = { }, 1;
-    local buff, icon, _, _, _, expirationTimer, _, _, _, spellID = UnitBuff("player", i);
+    local buffName, icon, expirationTimer, spellID = AZP.AddonHelper:GetBuffNameIconTimerID(i)
 
     local curTime = GetTime()
 
@@ -81,24 +87,24 @@ function addonMain:CheckConsumables(inputFrame)
         expirationTimer = floor((expirationTimer - curTime) / 60)
         i = i + 1;
         if addonMain:checkIfBuffInTable(spellID, AIU.buffs["Flask"]) then
-            currentFlask = {buff, spellID, expirationTimer, icon}
+            currentFlask = {buffName, spellID, expirationTimer, icon}
         elseif addonMain:checkIfBuffInTable(spellID, AIU.buffs["Food"]) then
-            currentFood = {buff, spellID, expirationTimer, icon}
+            currentFood = {buffName, spellID, expirationTimer, icon}
         elseif addonMain:checkIfBuffInTable(spellID, AIU.buffs["Rune"]) then
-            currentRune = {buff, spellID, expirationTimer, icon}
+            currentRune = {buffName, spellID, expirationTimer, icon}
         elseif addonMain:checkIfBuffInTable(spellID, AIU.buffs["RaidBuff"]) == "Intellect" then
-            currentInt = {buff, spellID, expirationTimer, icon}
+            currentInt = {buffName, spellID, expirationTimer, icon}
         elseif addonMain:checkIfBuffInTable(spellID, AIU.buffs["RaidBuff"]) == "Stamina" then
-            currentSta = {buff, spellID, expirationTimer, icon}
+            currentSta = {buffName, spellID, expirationTimer, icon}
         elseif addonMain:checkIfBuffInTable(spellID, AIU.buffs["RaidBuff"]) == "Attack Power" then
-            currentAtk = {buff, spellID, expirationTimer, icon}
+            currentAtk = {buffName, spellID, expirationTimer, icon}
         end
-        buff, icon, _, _, _, expirationTimer, _, _, _, spellID = UnitBuff("player", i);
+        buffName, icon, expirationTimer, spellID = AZP.AddonHelper:GetBuffNameIconTimerID(i)
     end
 
     local questionMarkIcon = "\124T134400:12\124t"
     if currentFlask == nil then
-        currentFlaskText = colorRed .. questionMarkIcon .. "NO FLASK!" .. colorEnd
+        currentFlaskText = colorRed .. questionMarkIcon .. " NO FLASK!" .. colorEnd
     else
         if currentFlask[3] <= 10 then
             currentFlaskText = colorYellow .. currentFlask[3] .. " minutes.".. colorEnd
@@ -109,7 +115,7 @@ function addonMain:CheckConsumables(inputFrame)
     end
 
     if currentFood == nil then
-        currentFoodText = colorRed .. questionMarkIcon .. "NO FOOD!" .. colorEnd
+        currentFoodText = colorRed .. questionMarkIcon .. " NO FOOD!" .. colorEnd
     else
         if currentFood[3] <= 10 then
             currentFoodText = colorYellow .. currentFood[3] .. " minutes.".. colorEnd
@@ -120,7 +126,7 @@ function addonMain:CheckConsumables(inputFrame)
     end
 
     if currentRune == nil then
-        currentRuneText = colorRed .. questionMarkIcon .. "NO RUNE!" .. colorEnd
+        currentRuneText = colorRed .. questionMarkIcon .. " NO RUNE!" .. colorEnd
     else
         if currentRune[3] <= 10 then
             currentRuneText = colorYellow .. currentRune[3] .. " minutes.".. colorEnd
@@ -131,7 +137,7 @@ function addonMain:CheckConsumables(inputFrame)
     end
 
     if currentInt == nil then
-        currentIntText = colorRed .. questionMarkIcon .. "NO INTELLECT!" .. colorEnd
+        currentIntText = colorRed .. questionMarkIcon .. " NO INTELLECT!" .. colorEnd
     else
         if currentInt[3] <= 10 then
             currentIntText = colorYellow .. currentInt[3] .. " minutes.".. colorEnd
@@ -142,7 +148,7 @@ function addonMain:CheckConsumables(inputFrame)
     end
 
     if currentSta == nil then
-        currentStaText = colorRed .. questionMarkIcon .. "NO STAMINA!" .. colorEnd
+        currentStaText = colorRed .. questionMarkIcon .. " NO STAMINA!" .. colorEnd
     else
         if currentSta[3] <= 10 then
             currentStaText = colorYellow .. currentSta[3] .. " minutes.".. colorEnd
@@ -153,7 +159,7 @@ function addonMain:CheckConsumables(inputFrame)
     end
 
     if currentAtk == nil then
-        currentAtkText = colorRed .. questionMarkIcon .. "NO ATTACK POWER!" .. colorEnd
+        currentAtkText = colorRed .. questionMarkIcon .. " NO ATTACK POWER!" .. colorEnd
     else
         if currentAtk[3] <= 10 then
             currentAtkText = colorYellow .. currentAtk[3] .. " minutes.".. colorEnd
@@ -163,10 +169,11 @@ function addonMain:CheckConsumables(inputFrame)
         currentAtkText = "\124T" .. currentAtk[4] .. ":12\124t " .. currentAtkText
     end
 
-    inputFrame:SetText(readyCheckDefaultText .. "\n\n" .. currentFlaskText .. "\n" .. currentFoodText .. "\n" .. currentRuneText .. "\n" .. currentIntText .. "\n" .. currentStaText .. "\n" .. currentAtkText)
+    inputFrame:SetText(readyCheckDefaultText)
+    BuffsLabel.contentText:SetText(currentFlaskText .. "\n" .. currentFoodText .. "\n" .. currentRuneText .. "\n" .. currentIntText .. "\n" .. currentStaText .. "\n" .. currentAtkText)
 end
 
-function OnEvent:ReadyCheckOE(event, ...)
+function AZP.IU.OnEvent:ReadyCheck(event, ...)
     if event == "READY_CHECK" then
         local player = arg1
         if (player ~= UnitName("player")) then
@@ -178,19 +185,4 @@ function OnEvent:ReadyCheckOE(event, ...)
             addonMain:CheckConsumables(ReadyCheckFrameText)
         end
     end
-end
-
-function addonMain:DelayedExecution(delayTime, delayedFunction)
-	local frame = CreateFrame("Frame")
-	frame.start_time = GetServerTime()
-	frame:SetScript("OnUpdate",
-		function(self)
-			if GetServerTime() - self.start_time > delayTime then
-				delayedFunction()
-				self:SetScript("OnUpdate", nil)
-				self:Hide()
-			end
-		end
-	)
-	frame:Show()
 end
