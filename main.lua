bcc6ac62ba0fa3aceb47881dc8dd13ae291f594d
Arco Dielhof <arcodielhof@hotmail.com>
2020-10-21 16:08:58 +0200

    AutoUpload Test2

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AIU = ...
 
-local AZPIUReadyCheckVersion = 12
+local AZPIUReadyCheckVersion = 13
 local dash = " - "
 local name = "InstanceUtility" .. dash .. "ReadyCheck"
 local nameFull = ("AzerPUG " .. name)
