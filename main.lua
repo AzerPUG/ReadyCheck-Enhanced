42925dd6b1ab339fb2a9ba5fd299bf9d7b600a65
Arco Dielhof <arcodielhof@hotmail.com>
2020-11-06 19:31:36 +0100

    Update .TOC file since some people were experience issues due to wrong wow version?

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AIU = ...
 
-local AZPIUReadyCheckVersion = 13
+local AZPIUReadyCheckVersion = 14
 local dash = " - "
 local name = "InstanceUtility" .. dash .. "ReadyCheck"
 local nameFull = ("AzerPUG " .. name)
