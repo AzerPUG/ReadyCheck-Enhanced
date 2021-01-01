7f833fb7056fdf5d450442f98a354158c4d332b0
Arco Dielhof <arcodielhof@hotmail.com>
2021-01-01 20:26:55 +0100

    Version++;

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local GlobalAddonName, AIU = ...
 
-local AZPIUReadyCheckVersion = 18
+local AZPIUReadyCheckVersion = 19
 local dash = " - "
 local name = "InstanceUtility" .. dash .. "ReadyCheck"
 local nameFull = ("AzerPUG " .. name)
