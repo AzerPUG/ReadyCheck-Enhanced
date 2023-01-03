d06cdc08c0482d04d042b5d18c8d80579fd63ff2
Arco Dielhof <arcodielhof@hotmail.com>
2023-01-03 23:53:46 +0100

    Version++;

--- ReadyCheckEnhanced.lua
+++ ReadyCheckEnhanced.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["ReadyCheck Enhanced"] = 66
+AZP.VersionControl["ReadyCheck Enhanced"] = 67
 if AZP.ReadyCheckEnhanced == nil then AZP.ReadyCheckEnhanced = {} end
 if AZP.ReadyCheckEnhanced.Events == nil then AZP.ReadyCheckEnhanced.Events = {} end
 
